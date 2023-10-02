5b34fefae8937a5a6ac44d4b33ebb05556c2cbae
Holly J. Morris <hmorris@npr.org>
2023-10-02 14:47:58 -0400

    all hte files

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# scraper-corrections
-Generating an RSS feed from https://www.npr.org/corrections
+# corrections-scraper
+ 
+This uses Beautiful Soup to scrape the https://www.npr.org/corrections/ page and convert it to an RSS feed that is used to run the NPR #corrections slack channel. 
+
+A Github action runs the script and checks for changes every hour. If it detects changes, it updates npr_corrections_rss.xml in the gh-pages branch.
+
+The feed can be accessed at TK. Changes will appear a minute or two after the action runs. floogie
---
+++ npr_corrections_scraper.py
@@ -0,0 +1,67 @@
+import requests
+from bs4 import BeautifulSoup
+from feedgen.feed import FeedGenerator
+from datetime import datetime
+import pytz
+
+# Timezone adjustment
+eastern_timezone = pytz.timezone('US/Eastern')
+current_utc_time = datetime.now(pytz.utc)
+current_et_time = current_utc_time.astimezone(eastern_timezone)
+
+# URL to scrape
+URL = 'https://www.npr.org/corrections/'
+
+# Fetch the page
+response = requests.get(URL)
+soup = BeautifulSoup(response.content, 'html.parser')
+
+# Initialize FeedGenerator
+fg = FeedGenerator()
+fg.id(URL)
+fg.title('NPR Corrections')
+fg.author({'name': 'NPR', 'email': 'hmorris@npr.org'})
+fg.link(href=URL, rel='alternate')
+fg.subtitle('Corrections from NPR')
+fg.language('en')
+fg.lastBuildDate(current_et_time)
+
+# Parse the corrections
+corrections = soup.find_all('div', class_='item-info')
+# ... (the rest of the imports and the beginning of your script remains unchanged)
+
+for correction in reversed(corrections):
+    entry = fg.add_entry()
+        
+    # Extracting title and link of the story
+    title_link = correction.find('h2', class_='title').find('a')
+    story_title = title_link.text.strip()
+    story_link = title_link['href']
+    
+    # Extracting the correction details
+    correction_content_div = correction.find('div', class_='correction-content')
+    corrected_on_text = correction_content_div.find('h3', class_='corrected-on').text.strip()
+    corrected_on_date_str = corrected_on_text.replace('Corrected on ', '')
+    corrected_on_date = datetime.strptime(corrected_on_date_str, '%Y-%m-%d %H:%M:%S')
+    formatted_date = corrected_on_date.strftime('%a, %d %b %Y %H:%M:%S -0500')  # RFC 822 format
+
+    correction_text = correction_content_div.find('p').text.strip()
+
+    # Adding to the RSS feed entry
+    entry.id(story_link)
+    entry.title(f"{story_title}")
+    entry.link(href=story_link, rel='alternate')
+    entry.published(formatted_date)  # Add the publication date
+    description_content = f"{correction_text}"
+    entry.description(description_content)
+
+
+# Generate RSS feed
+rssfeed = fg.rss_str(pretty=True)
+
+# Write the RSS feed to a file
+with open('npr_corrections_rss.xml', 'wb') as f:
+#with open('/home/runner/work/corrections-scraper/corrections-scraper/npr_corrections_rss.xml', 'wb') as f:
+    f.write(rssfeed)
+
+print("RSS feed generated and saved as 'npr_corrections_rss.xml'")
