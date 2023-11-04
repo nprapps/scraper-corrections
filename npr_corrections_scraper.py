011f68ef15b176333913d9d355595c4fccf351ef
Holly J. Morris <hmorris@npr.org>
2023-11-03 21:20:15 -0400

    simplified version

--- npr_corrections_scraper.py
+++ npr_corrections_scraper.py
@@ -6,7 +6,7 @@ import pytz
 import os
 import feedparser
 
-# Timezone adjustment (this can be removed if you're not using it)
+# Timezone adjustment
 eastern_timezone = pytz.timezone('US/Eastern')
 current_utc_time = datetime.now(pytz.utc)
 current_et_time = current_utc_time.astimezone(eastern_timezone)
@@ -28,9 +28,7 @@ fg.subtitle('Corrections from NPR')
 fg.language('en')
 fg.lastBuildDate(current_utc_time)
 
-# ... [Previous part of the code remains unchanged]
-
-# 1. Parse new corrections
+# Parse new corrections
 new_entries = []
 
 for correction in soup.find_all('div', class_='item-info')[:60]:
@@ -50,54 +48,27 @@ for correction in soup.find_all('div', class_='item-info')[:60]:
         'published': formatted_date
     })
 
-# 2. Read old entries from the existing RSS feed file
-old_feed_entries = []
+# Read old entries from the existing RSS feed file
+old_links = set()
 
 if os.path.exists('npr_corrections_rss.xml'):
     old_feed = feedparser.parse('npr_corrections_rss.xml')
     for entry in old_feed.entries:
-        old_feed_entries.append({
-            'title': entry.title,
-            'link': entry.link,
-            'description': entry.description,
-            'published': entry.published
-        })
+        old_links.add(entry.link)
 
-# Create a dictionary to store old entries by their links
-old_feed_map = {entry['link']: entry for entry in old_feed_entries}
+# Process and add the new entries, in reverse order to ensure reverse chronological
+counter = 0
+for entry_data in reversed(new_entries):
+    if counter >= 60:  # Limit to 60 items
+        break
 
-# 3. Process and add the new entries
-for entry_data in new_entries:
-    # If the link exists in old entries, check the description
-    if entry_data['link'] in old_feed_map:
-        # If descriptions differ, update and prepend
-        if entry_data['description'] != old_feed_map[entry_data['link']]['description']:
-            old_feed_map[entry_data['link']] = entry_data  # Update the entry in the map
-            entry = fg.add_entry(order='prepend')
-            entry.title(entry_data['title'])
-            entry.link(href=entry_data['link'], rel='alternate')
-            entry.description(entry_data['description'])
-            entry.published(entry_data['published'])
-            del old_feed_map[entry_data['link']]  # Remove the entry from the old_feed_map to prevent re-adding it
-    else:
-        # If the link doesn't exist, just prepend
-        entry = fg.add_entry(order='prepend')
+    if entry_data['link'] not in old_links:
+        entry = fg.add_entry()
         entry.title(entry_data['title'])
         entry.link(href=entry_data['link'], rel='alternate')
         entry.description(entry_data['description'])
         entry.published(entry_data['published'])
-
-# After processing the new entries, append all remaining old entries
-for link, entry_data in old_feed_map.items():
-    entry = fg.add_entry(order='append')
-    entry.title(entry_data['title'])
-    entry.link(href=entry_data['link'], rel='alternate')
-    entry.description(entry_data['description'])
-    entry.published(entry_data['published'])
-
-    # Ensure you don't exceed 60 total entries in the feed
-    if len(fg.entry()) >= 60:
-        break
+        counter += 1
 
 # Generate RSS feed
 rssfeed = fg.rss_str(pretty=True)
